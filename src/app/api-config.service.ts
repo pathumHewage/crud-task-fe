1a07dd720ad1c557c4633540d021bbf1b5422e36
pathumHewage <it20185220@my.sliit.lk>
2022-01-03 22:01:16 +0530

    add Task model classes

--- src/app/api-config.service.ts
+++ src/app/api-config.service.ts
@@ -6,7 +6,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiConfigService {
 
-  ROOT_URL = '';
+  API_BASE_URL = 'http://localhost:3000';
+  constructor(private httpClient: HttpClient) { }
+  //API call to backend
+  get(url: string){
+    return this.httpClient.get(`${this.API_BASE_URL}/${url}`);
+  }
 
-  constructor() { }
+  post(url: string, data: object){
+    return this.httpClient.post(`${this.API_BASE_URL}/${url}`,data);
+  }
+
+  put(url: string, data: object){
+    return this.httpClient.put(`${this.API_BASE_URL}/${url}`,data);
+  }
+
+  patch(url: string, data: object){
+    return this.httpClient.patch(`${this.API_BASE_URL}/${url}`,data);
+  }
+
+  delete(url: string){
+    return this.httpClient.delete(`${this.API_BASE_URL}/${url}`);
+  }
+
+
 }
